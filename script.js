991614a79387467f358c25b10313e130e9c5c4be
yashraj1405 <yashrajkadam1405@gmail.com>
2024-10-20 21:53:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-open');
+});
